e9a0fe30b2cccc556d2280b31745353a8e672a65
Eros <erosp2017@gmail.com>
2024-09-26 06:51:40 -0500

    instalacion y configuracion de vue

---
+++ resources/js/components/About.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Acerca de</h1>
+        <router-link to="/">Ir a Inicio</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'About',
+}
+</script>
---
+++ resources/js/components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Página de Inicio</h1>
+        <router-link to="/about">Ir a Acerca de</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+}
+</script>
